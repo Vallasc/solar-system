b97b494341cf14304a00a796f81deb0fe7b199ec
alleSini99 <72938497+alleSini99@users.noreply.github.com>
2020-10-17 13:01:03 +0200

    Update Body.h

--- simulator/Body.h
+++ simulator/Body.h
@@ -17,12 +17,11 @@ class Body{
     void updateVelocity(double t);
     void updatePosVel(double t);
 
-
     static int deadBodies;
     double position[2];
     double acceleration[2]; 
-    double radius;
     double velocity[2];
+    double radius
     double mass;
     //double E_k=0.5*mass*(pow(velocity[0], 2)+pow(velocity[1], 2));
     //double E_t;
