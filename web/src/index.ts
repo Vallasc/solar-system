566e0da2f5448779bca6652e7b567b01d900ffb8
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-10-27 11:12:04 +0100

    Initial commit

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,6 @@
-let words: Array<string> = ['Hello2', 'World'];
-for (let word of words) {
-    console.log(word);
-}
+const canvas : HTMLCanvasElement = document.getElementById("main-canvas") as HTMLCanvasElement;
+const ctx : CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+ctx.beginPath();
+ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+ctx.stroke();
