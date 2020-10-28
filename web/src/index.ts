ebdc8b4d7dd4bdd7a19400a49d96b3bbfe19c902
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-10-28 18:13:49 +0100

    Add event listener

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,8 @@ class Startup {
 
     static mainCanvas : HTMLCanvasElement;
     static sideContainer : HTMLElement;
-    static loop : Loop;
+    static loop : Loop | null;
+    static bodies : Array<Array<Body>> = [];
 
     public static main(): number {
         console.log('Main');
@@ -11,18 +12,37 @@ class Startup {
         window.onresize = Startup.onWindowResized;
         Startup.resize();
 
-        let array : Array<Array<Body>> = [];
+        /*let array : Array<Array<Body>> = [];
         for(let i=0; i<60*60; i++){
             let row = [];
             for(let j=0; j<5; j++)
                 row.push(new Body({ x : j*100+i, y: j*100+i}));
             array.push(row);
-        }
-        console.log(array);
-        Startup.loop = new Loop(Startup.mainCanvas, array);
+        }*/
+        const fileSelector = <HTMLElement> document.getElementById('file-selector');
+        fileSelector.addEventListener('change', (event: Event) => {
+            const target= event.target as HTMLInputElement;
+            const file: File = (target.files as FileList)[0];
 
-        let d = new Deserializer();
-        d.getJson("bodies.json");
+            let fr = new FileReader(); 
+            fr.onload=function(){ 
+                Startup.bodies = Deserializer.parseJson(<string> fr.result);
+                console.log(Startup.bodies);
+                Startup.loop = null;
+                Startup.loop = new Loop(Startup.mainCanvas, Startup.bodies);
+            } 
+              
+            fr.readAsText(file); 
+        });
+        const buttonPlay = <HTMLElement> document.getElementById("play-button");
+        buttonPlay.onclick = ()=>{
+            Startup.loop!.reset();
+        }
+        //console.log(array);
+        //Startup.loop = new Loop(Startup.mainCanvas, array);
+
+        //let d = new Deserializer();
+        //d.getJson("bodies.json");
         
         return 0;
     }
@@ -32,7 +52,7 @@ class Startup {
     }
 
     public static resize ():void {
-        Startup.mainCanvas.width = window.innerWidth*0.7;
+        Startup.mainCanvas.width = window.innerWidth*0.8;
         Startup.mainCanvas.height = window.innerHeight;
     }
 
