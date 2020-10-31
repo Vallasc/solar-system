4deaefc7bfb48be8ef5883da8756da008112d58d
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-10-31 18:23:23 +0100

    Clean output json

--- simulator/serializer.cpp
+++ simulator/serializer.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 
 #include "serializer.h"
 
@@ -10,27 +11,38 @@ Serializer::Serializer(string file_name){
 
     outfile.open(file_name);
     cout << "File opened" << endl;
-    outfile << "{" << endl << "\"states\": [" << endl;
+    outfile << "{\"states\":[" << endl;
 }
 
 Serializer::~Serializer(){
-    outfile << "]" << endl << "}" << endl;
+    outfile << endl << "]}";
     outfile.close();
     cout << "File closed" << endl;
 }
 
 
 void Serializer::write(float time, vector<Body> &state){ // Si possono aggiungere altri parametri da salvare
+    std::stringstream ss2;
+    ss2 << "\"p\":[";
+    for(int i = 0; i < state.size(); i++) {
+        if(i > 0)
+            ss2 << ",";
+        ss2 << state[i].to_json();
+    }
+    ss2 << "]" << "}";
+    string out2 = ss2.str();
+
+    std::stringstream ss1;
+    int ss1_length = 16;
+    // Calcolo al lunghezza dell'oggetto json completo di virgola e \n
+    ss1 << "{" << "\"s\":\"" << setfill('0') << setw(8) << out2.length() + ss1_length << "\",";
+
+    string out1 = ss1.str();
+
     if( first ) 
         first = false;
     else 
-        outfile << "," << endl;
-    outfile << "{" << "\"time\": " << time << ", ";
-    outfile << "\"particles\": [";
-    for(int i = 0; i < state.size(); i++) {
-        if(i > 0)
-            outfile << ",";
-        outfile << state[i].to_json();
-    }
-    outfile << "]" << "}";
+        outfile << ",";// << endl;
+
+    outfile << out1 << out2;
 }
