1578b241685ede29efc830c379c82e97e36b9642
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-11-02 14:33:26 +0100

    Built

--- web/dist/bundle/bundle.js
+++ web/dist/bundle/bundle.js
@@ -56,17 +56,18 @@ class Deserializer {
         let numParams = 5;
         try {
             let json = JSON.parse(blob);
+            console.log(json);
             let fifo = new Fifo();
             for (let i = 0; i < json.states.length; i++) {
                 let len = json.states[i].p.length + 1;
                 let objects = new Float64Array(len * numParams);
                 objects[0] = len;
                 for (let j = 0; j < len - 1; j++) {
-                    objects[(j + 1) * numParams] = json.states[i].p[j].x;
-                    objects[(j + 1) * numParams + 1] = json.states[i].p[j].y;
-                    objects[(j + 1) * numParams + 2] = json.states[i].p[j].r;
-                    objects[(j + 1) * numParams + 3] = json.states[i].p[j].k;
-                    objects[(j + 1) * numParams + 4] = json.states[i].p[j].i;
+                    objects[(j + 1) * numParams] = Math.ceil(json.states[i].p[j].x);
+                    objects[(j + 1) * numParams + 1] = Math.ceil(json.states[i].p[j].y);
+                    objects[(j + 1) * numParams + 2] = Math.ceil(json.states[i].p[j].r);
+                    objects[(j + 1) * numParams + 3] = Math.ceil(json.states[i].p[j].k);
+                    objects[(j + 1) * numParams + 4] = Math.ceil(json.states[i].p[j].i);
                 }
                 fifo.push(objects);
             }
