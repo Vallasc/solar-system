4c004e6810b83f849a3e140d9f66cbec621d7219
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-11-02 13:03:56 +0100

    Add error catching

--- web/src/main/deserializer.ts
+++ web/src/main/deserializer.ts
@@ -10,24 +10,28 @@ class Deserializer {
 
     public static parseJsonFloat64Array(blob: string): Fifo<any> {
       let numParams = 5;
-      let json = JSON.parse(blob);
-      let fifo: Fifo<any> = new Fifo();
-      for(let i = 0; i<json.states.length; i++) {
+      try{
+        let json = JSON.parse(blob);
+        let fifo: Fifo<any> = new Fifo();
+        for(let i = 0; i<json.states.length; i++) {
 
-        let len = json.states[i].p.length+1;
-        let objects = new Float64Array(len*numParams);
+          let len = json.states[i].p.length+1;
+          let objects = new Float64Array(len*numParams);
 
-        objects[0] = len;
-        for(let j=0; j<len-1; j++){
-          objects[(j+1) * numParams] = json.states[i].p[j].x;
-          objects[(j+1) * numParams + 1] = json.states[i].p[j].y;
-          objects[(j+1) * numParams + 2] = json.states[i].p[j].r;
-          objects[(j+1) * numParams + 3] = json.states[i].p[j].k;
-          objects[(j+1) * numParams + 4] = json.states[i].p[j].i;
+          objects[0] = len;
+          for(let j=0; j<len-1; j++){
+            objects[(j+1) * numParams] = json.states[i].p[j].x;
+            objects[(j+1) * numParams + 1] = json.states[i].p[j].y;
+            objects[(j+1) * numParams + 2] = json.states[i].p[j].r;
+            objects[(j+1) * numParams + 3] = json.states[i].p[j].k;
+            objects[(j+1) * numParams + 4] = json.states[i].p[j].i;
+          }
+
+          fifo.push(objects);
         }
-
-        fifo.push(objects);
+        return fifo;
+      } catch (e) {
+        throw Error("Failed parsing file");
       }
-      return fifo;
     }
 }
