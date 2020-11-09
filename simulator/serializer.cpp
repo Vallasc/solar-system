f7aba597f8d4d6a45d8df55b1f253c9d7fd068df
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-11-09 18:58:24 +0100

    Add file compression

--- simulator/serializer.cpp
+++ simulator/serializer.cpp
@@ -3,39 +3,67 @@
 #include <iomanip>
 
 #include "serializer.h"
+#include "miniz.h"
 
 using namespace std;
 
-/*Serializer::Serializer(string file_name){
+Serializer::Serializer(string file_name) {
+    this->byte_written = 0;
+    this->name_index = 0;
+    this->num_iteration = 0;
     this->file_name = file_name;
-
-    outfile.open(file_name);
+    this->open_file();
     cout << "File opened" << endl;
 }
 
-Serializer::~Serializer(){
+Serializer::~Serializer() {
     outfile.close();
     cout << "File closed" << endl;
+    this->compress_files();
+    cout << "Iterations: " << this->num_iteration << endl;
+    cout << "Minutes of simulation: " << this->num_iteration/60/60 << endl;
 }
 
 
-void Serializer::write(float time, vector<Body> &state){ // Si possono aggiungere altri parametri da salvare
-    std::stringstream ss2;
+void Serializer::write(float time, vector<Body> &state) { // Si possono aggiungere altri parametri da salvare
+    float size = (float) state.size();
+    outfile.write(reinterpret_cast<char*>(& size), sizeof(float));
+    this->byte_written += sizeof(float);
     for(int i = 0; i < state.size(); i++) {
-        ss2 << state[i].serialize();
+        this->byte_written += state[i].write_to_file(outfile);
     }
-    string out2 = ss2.str();
+    if(this->byte_written >= this->max_file_size){
+        outfile.close();
+        this->byte_written = 0;
+        this->open_file();
+    }
+    this->num_iteration++;
+}
 
-    std::stringstream ss1;
-    int ss1_length = 9;
-    // Calcolo al lunghezza dell'oggetto json completo di virgola e \n
-    ss1 << setfill('0') << setw(8) << out2.length() + ss1_length << " ";
+void Serializer::open_file() {
+    string f_name = this->file_name + std::to_string(this->name_index++) + ".bin";
+    outfile.open(f_name, std::ios::binary);
+}
 
-    string out1 = ss1.str();
+void Serializer::compress_files() {
+    cout << "Compressing" << endl;
+    // ===== Prepare an archive file;
+    mz_zip_archive archive = mz_zip_archive();
+    mz_zip_writer_init_file(&archive, (this->file_name + ".zip").c_str(), 0);
 
-    outfile << out1 << out2;
-}*/
+    for(int i=0; i<name_index; i++) {
+        string f_name = this->file_name + std::to_string(i) + ".bin";
+        mz_zip_writer_add_file(&archive, Serializer::get_base_name(f_name).c_str(), f_name.c_str(), 0, 0, this->file_compression);
+        remove(f_name.c_str());
+    }
 
+    // ===== Finalize and close the temporary archive
+    mz_zip_writer_finalize_archive(&archive);
+    mz_zip_writer_end(&archive);
+    cout << "Done" << endl;
+}
+
+/*
 Serializer::Serializer(string file_name){
     this->file_name = file_name;
 
@@ -75,3 +103,4 @@ void Serializer::write(float time, vector<Body> &state){ // Si possono aggiunger
 
     outfile << out1 << out2;
 }
+*/
--- simulator/serializer.h
+++ simulator/serializer.h
@@ -14,7 +14,18 @@ class Serializer {
     private:
         ofstream outfile;
         string file_name;
-        bool first = true;
+        int byte_written;
+        int name_index;
+        int num_iteration;
+        const int max_file_size = 2000000; // 1MB
+        const int file_compression = 10; // 1 low - 10 max
+
+        static string get_base_name(string const & path)
+        {
+            return path.substr(path.find_last_of("/\\") + 1);
+        }
+        void open_file();
+        void compress_files();
 
     public:
         Serializer(string file_name);
