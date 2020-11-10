19e02cb1d7525dd746287b1e089fa100fdeb3437
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-11-10 10:16:09 +0100

    Add id

--- web/src/main/body.ts
+++ web/src/main/body.ts
@@ -1,15 +1,18 @@
 class Body {
+    public id: number = 0;
     public x: number = 0;
     public y: number = 0;
     public radius: number = 0;
     public k_energy: number = 0;
     public internal_energy: number = 0;
 
-    constructor( {  x = 0, 
+    constructor( {  id = 0,
+                    x = 0, 
                     y = 0, 
                     radius = 1,
                     k_energy = 0,
                     internal_energy = 0} = {}) {
+        this.id = id;
         this.x = x;
         this.y = y;
         this.radius = radius;
