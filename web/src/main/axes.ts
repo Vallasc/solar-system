077890609e84029d02b6c192da8ed98eb2caaefe
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-11-11 17:40:00 +0100

    Fix infinite ticks

--- web/src/main/axes.ts
+++ web/src/main/axes.ts
@@ -15,7 +15,8 @@ class Axes {
         
         let w : number = this.canvas.width;
         let h : number = this.canvas.height;
-        let dist_grids : number = 10; //distance between grids
+        let distGrids : number = 10; //distance between grids
+        let bigEvery : number = 5; // 1 big every 10 small
         let offX : number = 0;
         let offY : number = 0;
 
@@ -37,7 +38,7 @@ class Axes {
             
         this.context.clearRect(0, 0, w, h);
 
-        this.context.strokeStyle = "rgba(255,0,0,0.5)"; 
+        this.context.strokeStyle = "rgba(255,0,0,0.8)"; 
         this.context.lineWidth = 2;
         // Draw >  
         this.context.beginPath();
@@ -67,64 +68,74 @@ class Axes {
         this.context.lineTo(offX + w*0.5, h);
         this.context.stroke();
         
-        
-        // Ticks marks along the positive X-axis
-        for(let i=0; i<Math.round(w*0.5); i++) {
-            this.context.beginPath();
-            
-            // Draw a tick mark 5px long (-4 to 4)
-          //  if (offX != w*0.5-margin) {
-          //      this.context.moveTo(offX + w*0.5+i*dist_grids, offY + h*0.5-4);
-          //      this.context.lineTo(offX + w*0.5+i*dist_grids, offY + h*0.5+4);
-          //  } else {
-                this.context.moveTo(this.panningOffsetX + w*0.5+i*dist_grids, offY + h*0.5-4);
-                this.context.lineTo(this.panningOffsetX + w*0.5+i*dist_grids, offY + h*0.5+4);
-          //  }
-            
-            this.context.stroke();
-        
-            
-            // Text value at that point
-            //this.context.font = '100 20px Arial';
-            //this.context.fillStyle = "red";
-            //this.context.fillText("m", offX + w*0.5 - 30, 15);
-            
-        }
+        this.context.lineWidth = 1;
+
+        let bigTick = 6;
+        let smallTick = 3;
+
+        let newW = Math.floor(w/2) - (Math.floor(w/2) % (distGrids*bigEvery));
+        let xNumTick = (newW / distGrids);
 
         // Ticks marks along the negative X-axis
-        for(let i=Math.round(w*0.5); i>0; i--) {
+        for(let i=0; i<xNumTick ; i++) {
             this.context.beginPath();
-
-            // Draw a tick mark 5px long (-4 to 4)
-            this.context.moveTo(this.panningOffsetX + w*0.5-i*dist_grids, offY + h*0.5-4);
-            this.context.lineTo(this.panningOffsetX + w*0.5-i*dist_grids, offY + h*0.5+4);
-            this.context.stroke();            
-        }
-
-        // Ticks marks along the positive Y-axis
-        for(let i=1; i<Math.round(h*0.5); i++) {
-            this.context.beginPath();
-
-            // Draw a tick mark 5px long (-4 to 4)
-            this.context.moveTo(offX + w*0.5-4, this.panningOffsetY + h*0.5-i*dist_grids);
-            this.context.lineTo(offX+ w*0.5+4, this.panningOffsetY + h*0.5-i*dist_grids);
+            let mod = (this.panningOffsetX + i*distGrids) % (newW);
+            mod = mod < 0 ? (newW + mod) : mod; // Riporto il modulo positivo
+            if(i % bigEvery == 0){
+                this.context.moveTo(w/2 + mod, offY + h/2 - bigTick);
+                this.context.lineTo(w/2 + mod, offY + h/2 + bigTick);
+            } else {
+                this.context.moveTo(w/2 + mod, offY + h/2 - smallTick);
+                this.context.lineTo(w/2 + mod, offY + h/2 + smallTick);
+            }
             this.context.stroke();
-
-            
-            // Text value at that point
-            //this.context.font = '100 20px Arial';
-            //this.context.fillStyle = "red";
-            //this.context.fillText("m", w - 30, offY + h*0.5 + 30);
-            
         }
+
+        // Ticks marks along the positive X-axis
+        for(let i=xNumTick; i>0; i--) {
+            this.context.beginPath();
+            let mod = (i*distGrids - this.panningOffsetX) % (newW);
+            mod = mod < 0 ? (newW + mod) : mod; // Riporto il modulo positivo
+            if((i) % bigEvery == 0){
+                this.context.moveTo(w/2 - mod, offY + h/2 - bigTick);
+                this.context.lineTo(w/2 - mod, offY + h/2 + bigTick);
+            } else {
+                this.context.moveTo(w/2 - mod, offY + h/2 - smallTick);
+                this.context.lineTo(w/2 - mod, offY + h/2 + smallTick);
+            }
+            this.context.stroke();
+        }
+
+        let newH = Math.floor(h/2) - (Math.floor(h/2) % (distGrids*bigEvery));
+        let yNumTick = (newH / distGrids);
 
         // Ticks marks along the negative Y-axis
-        for(let i=Math.round(h*0.5); i>0; i--) {
+        for(let i=0; i<yNumTick ; i++) {
             this.context.beginPath();
-
-            // Draw a tick mark 5px long (-4 to 4)
-            this.context.moveTo(offX + w*0.5-4, this.panningOffsetY + h*0.5+i*dist_grids);
-            this.context.lineTo(offX + w*0.5+4, this.panningOffsetY + h*0.5+i*dist_grids);
+            let mod = (this.panningOffsetY + i*distGrids) % (newH);
+            mod = mod < 0 ? (newH + mod) : mod; // Riporto il modulo positivo
+            if(i % bigEvery == 0){
+                this.context.moveTo(offX + w/2 - bigTick, h/2 + mod);
+                this.context.lineTo(offX + w/2 + bigTick, h/2 + mod);
+            } else {
+                this.context.moveTo(offX + w/2 - smallTick, h/2 + mod);
+                this.context.lineTo(offX + w/2 + smallTick, h/2 + mod);
+            }
+            this.context.stroke();
+        }
+        
+        // Ticks marks along the positive Y-axis
+        for(let i=yNumTick; i>0; i--) {
+            this.context.beginPath();
+            let mod = (i*distGrids - this.panningOffsetY) % (newH);
+            mod = mod < 0 ? (newH + mod) : mod; // Riporto il modulo positivo
+            if(i % bigEvery == 0){
+                this.context.moveTo(offX + w/2 - bigTick, h/2 - mod);
+                this.context.lineTo(offX + w/2 + bigTick, h/2 - mod);
+            } else {
+                this.context.moveTo(offX + w/2 - smallTick, h/2 - mod);
+                this.context.lineTo(offX + w/2 + smallTick, h/2 - mod);
+            }
             this.context.stroke();
         }
     }
@@ -134,4 +145,5 @@ class Axes {
         this.panningOffsetY = y;
         this.drawAxes();
     }
+
 }
