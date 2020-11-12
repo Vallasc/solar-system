bba4ac62c1371ade3e0af06336289b4fc222e529
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2020-11-12 17:07:38 +0100

    Change array split

--- web/src/main/deserializer.ts
+++ web/src/main/deserializer.ts
@@ -2,19 +2,21 @@ declare var zip : any;
 zip.workerScriptsPath = "./dist/lib/zipjs/";
 
 class Deserializer {
-    static readonly bodyNumParams = 6;
+    static readonly bodyNumParams = 5;
+    static readonly numIterationParam = 6; // 5 energie + size
 
     public static parseBinaryFloat32Array(blob: ArrayBuffer): Fifo<Float32Array> {
       let floatArray = new Float32Array(blob);
       let fifo: Fifo<Float32Array> = new Fifo();
 
       let objects : Float32Array;
-      let last : Float32Array | null = null;
-      let itLen = 0;
+      //let last : Float32Array | null = null;
+      let offset = 0;
       try{
-        for(let i=0; i<floatArray.length; i= i+Deserializer.bodyNumParams*itLen+1) {
-          itLen = floatArray[i];
-          objects = new Float32Array(floatArray.slice(i,i+Deserializer.bodyNumParams*itLen+1))
+        for(let i=(Deserializer.numIterationParam-1); i<floatArray.length; i= i + offset) {
+          offset = Deserializer.bodyNumParams*floatArray[i] + Deserializer.numIterationParam;
+          //console.log(floatArray[i]);
+          objects = new Float32Array(floatArray.slice(i-(Deserializer.numIterationParam-1), i + offset))
           fifo.push(objects);
           //console.log(objects);
         }
